e8e6f1559d2c5640821835a4bfba425b98777169
George <gkkaranja2@gmail.com>
2026-04-26 17:29:31 +0300

    Created the files to handle different exceptions

---
+++ src/main/java/ke/gekika/backend/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package ke.gekika.backend.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ke/gekika/backend/exceptions/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package ke.gekika.backend.exceptions;
+
+public class InvalidTokenException extends RuntimeException{
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
